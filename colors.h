c9f9af108ae71a9538bbc1d15a6fb1451742c8c0
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-07-17 10:55:43 -0400

    Add DWM Core

---
+++ colors.h
@@ -0,0 +1,11 @@
+// holds colors used for dwm
+static const char col_white[]       = "#ffffff";
+static const char col_black[]       = "#101010";
+static const char col_gray1[]       = "#0f0f0f";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_blue1[] 	    = "#1071b2";
+static const char col_blue2[]	    = "#224488";
+static const char col_green[]       = "#295340";
+static const char col_purpl[]       = "#725ddd";
